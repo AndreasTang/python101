97b6c50bc2bc78ae5e811fd1a7666987e8ae2e1d
AndreasTang <sbandy3131@gmail.com>
2019-10-09 11:48:04 +0800

    10/9 class update

---
+++ classTwo.py
@@ -0,0 +1,22 @@
+student = {[
+    'name': 'Nerine',
+    'chinese': 75,
+    'english': 96,
+    'math': 77
+], [
+    'name': 'Andreas',
+    'chinese': 70,
+    'english': 93,
+    'math': 58
+], [
+    'name': 'Chris',
+    'chinese': 88,
+    'english': 91,
+    'math': 44
+]}
+
+for i in range(len(student))
+
+    print(student[i][name],'has Scroe of: chinese:',student[i][chinese],' english: 'student[i][english],' math: ',student[i][math])
+
+
---
+++ classTwo_if.py
@@ -0,0 +1,26 @@
+scores = [77, 89, 34, 68, 56, 36, 67, 92 ,88 , 85]
+
+for score in scores:
+
+    if score < 60:
+
+        print('F')
+
+    else:
+
+        if score >= 90:
+
+            print('A')
+
+        elif score >= 80:
+
+            print('B')
+
+        elif score >= 70:
+
+            print('C')
+
+        elif score >= 60:
+
+            print('D')
+        
