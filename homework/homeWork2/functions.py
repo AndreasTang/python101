611fef91a4c76ca59d56f42d37a7decc9fa4cdda
AndreasTang <sbandy3131@gmail.com>
2019-10-29 20:42:41 +0800

    10/29 work complete

--- homework/homeWork2/functions.py
+++ homework/homeWork2/functions.py
@@ -1,6 +1,39 @@
-import menu
 
-def individualOrder(meal):
+from menu import menu
+import json
+import copy
+import os
+import glob
+
+
+
+def mealsOrderSystem():
+
+    mealsOrder = copy.deepcopy(menu)
+
+    print('Welcome to Full Belly steak house order system')
+    print('Please enter your name and phone number first')
+
+    customerName = raw_input('Your name: ')
+    customPhoneNumber = raw_input('Your phone number: ')
+
+    print('For order meals, Please enter the number of meal you want to order individual, we only accept integer')
+
+    for i in range(len(mealsOrder)):
+
+        meals = orderNumbers(mealsOrder[i]['meals'])
+        mealsOrder[i]['counter'] = meals
+
+    mealsOrder.insert(0, {
+            'name': customerName,
+            'phoneNumber': customPhoneNumber
+        })
+
+    return mealsOrder
+
+
+
+def orderNumbers(meal):
 
     isInteger = False
 
@@ -8,15 +41,19 @@ def individualOrder(meal):
 
         try:
 
-            individualMeals = float(input('How many {:s} you want to order: '.format(meal)))
+            orderNumber = str(input('How many {:s} you want to order: '.format(meal)))
 
-            if "." in individualMeals:
+            if "." in orderNumber:
 
                 print('Invalid input, we only accept integer, please try again')
             
+            elif '-' in orderNumber:
+
+                print('Invalid input, we only accept postive integer, please try again')
+
             else:
 
-                individualMeals = int(individualMeals)
+                orderNumber = int(orderNumber)
                 isInteger = True
 
         
@@ -24,26 +61,167 @@ def individualOrder(meal):
 
             print('Invalid input, we only accept integer, please try again')
 
-    return  individualMeals
+    return  orderNumber
         
 
 
-def mealsOrderSystem():
+def errorMessage():
 
-    mealsOrder = map(menu)
+    print('The number you type is neither 1 nor 0, did you mean to exit(type -1) ?')
 
-    print('Welcome to Full Belly steak house order system')
-    print('Please enter your name and phone number first')
 
-    customerName = input('Your name: ')
-    customPhoneNumber = input('Your phone number: ')
 
-    print('For order meals, Please enter the number of meal you want to order individual, we only accept integer')
-    print('You can also enter -1 to skip the rest of order process anytime if any orders are made, this will lead you to bill section')
+def mealsCounterSystem():
+    
+    print('welcome to meals counter system')
+    print('For count all bills please type 1')
+    print('For list all bills please type 2')
+    print('For delete bills please type 3')
 
-    for i in range(len(menu)):
+    isPass = False
 
-        meals = individualOrder(mealsOrder[meals][i])
-        mealsOrder[counter][i] = meals
+    while isPass == False:
 
-    return mealsOrder
+        codeSelection = int(input('What do you want ? : '))
+
+        if codeSelection == 1:
+
+            isPass = True
+            totalBills = countAllBill()
+            printResult(totalBills)
+            ending = raw_input('Enter anything to end: ')
+
+
+        elif codeSelection == 2:
+
+            isPass = True
+            listAllBill()
+            ending = raw_input('Enter anything to end: ')
+
+        elif codeSelection == 3:
+
+            isPass = True
+            fileName = raw_input('Enter the file name which you want to delete: ')
+            deleteBill(fileName)
+            ending = raw_input('Enter anything to end: ')
+
+        elif codeSelection == -1:
+
+            isPass = True
+            print('Exited')
+
+        else:
+
+            errorMessage()
+
+
+
+def countAllBill():
+
+    dir = os.path.dirname(__file__)
+    workPath = os.path.join(dir, './orders')
+    totalOrders = copy.deepcopy(menu)
+
+    try:
+
+        for filePath in glob.glob(os.path.join(workPath, '*.json')):
+
+            with open(filePath, 'r') as f:
+
+                mealsOrder = json.load(f)
+
+            for i in range(len(totalOrders)):
+
+                totalOrders[i]['counter'] += mealsOrder[i+1]['counter']
+
+    except OSError as e:
+
+        print(e)
+
+    else:
+
+        return totalOrders
+
+
+
+def listAllBill():
+
+    dir = os.path.dirname(__file__)
+    workPath = os.path.join(dir, './orders')
+    indexNumber = 0
+
+    try:
+
+        for filePath in glob.glob(os.path.join(workPath, '*.json')):
+
+            indexNumber += 1 
+
+            with open(filePath, 'r') as f:
+            
+                mealsOrder = json.load(f)
+
+                print('{:d}.{:s}: {:s}=>{:d}, {:s}=>{:d}, {:s}=>{:d}, {:s}=>{:d}'
+                .format(indexNumber,
+                mealsOrder[0]['name'],
+                mealsOrder[1]['meals'],
+                mealsOrder[1]['counter'],
+                mealsOrder[2]['meals'],
+                mealsOrder[2]['counter'],
+                mealsOrder[3]['meals'],
+                mealsOrder[3]['counter'],
+                mealsOrder[4]['meals'],
+                mealsOrder[4]['counter'],))
+
+    except OSError as e:
+        
+        print(e)
+    
+    else:
+
+        print('List complete')
+
+
+
+def deleteBill(fileName):
+
+    dir = os.path.dirname(__file__)
+    filePath = os.path.join(dir, './orders/{:s}.json'.format(fileName))
+
+    try:
+
+        os.remove(filePath)
+
+    except OSError as e:
+
+        print(e)
+
+    else:
+
+        print('File name {:s} have been deleted'.format(fileName))
+
+
+
+def writeOrders(name, orderData):
+
+    dir = os.path.dirname(__file__)
+    filePath = os.path.join(dir, './orders/{:s}'.format(name))
+
+    with open('{:s}.json'.format(filePath), 'w+') as f:
+        json.dump(orderData, f)
+
+
+def printResult(totalBills):
+
+    totalEarned = 0
+    sortedBills = sorted(totalBills, key = lambda i: i['counter'], reverse=True)
+
+    print('The meals sell from high to low is: ')
+
+    for i in range(len(sortedBills)):
+
+        earned = sortedBills[i]['price'] * sortedBills[i]['counter']
+        totalEarned += earned 
+        print('{:s}: {:d} => Earned: {:d}'.format(sortedBills[i]['meals'], sortedBills[i]['counter'], earned))
+
+    print('And the money we totally earn are: ')
+    print(totalEarned)
--- homework/homeWork2/main.py
+++ homework/homeWork2/main.py
@@ -2,30 +2,41 @@
 # list how many meals are order and how much money we earn
 # sort the order from high to low
 
+from functions import mealsOrderSystem
+from functions import mealsCounterSystem
+from functions import errorMessage
+from functions import writeOrders
+
+isPass = False
+
 print('Welcome to Full Belly steak house!!')
-print('You can enter 1 to enter meals order system or enter 2 to enter meals counter system')
+print('You can enter 1 to enter meals order system or enter 2 to enter meals counter system, -1 to exit system')
 
-codeSelection = int(input('What do you want ? (1 for meals order / 2 for meals counter) :'))
+while isPass == False:
 
-if codeSelection == 1:
+    codeSelection = int(input('What do you want ? (1 for meals order / 2 for meals counter / -1 to exit) : '))
 
-    mealsOrderSystem()
+    if codeSelection == 1:
 
-elif codeSelection == 2:
+        isPass = True
+        mealsOrder =  mealsOrderSystem()
+        print(mealsOrder)
+        writeOrders(mealsOrder[0]['name'], mealsOrder)
 
-    mealsCounterSystem()
+    elif codeSelection == 2:
 
-else:
+        isPass = True
+        mealsCounterSystem()
 
-    errorMessage()
+    elif codeSelection == -1:
+
+        isPass = True
+        print('Exited')
+
+    else:
+
+        errorMessage()
 
 
 
-print(mealsOrder)
-
-
-    # beefSteak = int(input('How many beefSteak you want to order: '))
-    # porkSteak = int(input('How many porkSteak you want to order: '))
-    # fishSteak = int(input('How many fishSteak you want to order: '))
-    # beefAndSeafoodSteak = int(input('How many beefAndSeafoodSteak you want to order: '))
 
